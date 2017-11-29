d89556dab0caf1b8518a2f9036e1f912f16adcea
Ed Kenschaft <ekenschaft@basistech.com>
2017-11-29 13:58:41 -0500

    No jira: Made key param optional

--- examples/sentences.py
+++ examples/sentences.py
@@ -29,7 +29,7 @@ def run(key, alt_url='https://api.rosette.com/rest/v1/'):
 PARSER = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                  description='Calls the ' +
                                  os.path.splitext(os.path.basename(__file__))[0] + ' endpoint')
-PARSER.add_argument('-k', '--key', help='Rosette API Key', required=True)
+PARSER.add_argument('-k', '--key', help='Rosette API Key')
 PARSER.add_argument('-u', '--url', help="Alternative API URL",
                     default='https://api.rosette.com/rest/v1/')
 
